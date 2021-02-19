4fa426c2f8ca6fb2b20ba551af3bda77347f0ea4
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-02-19 14:17:08 +0100

    add flushed

--- plugins/mkutils.hpp
+++ plugins/mkutils.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <cmath>
 namespace mkutils {
 
 // Constrain a number between a minimum and a maxmimum
@@ -11,5 +12,14 @@ template <class Number> Number inline constrain(Number input, Number min, Number
   } else {
     return input;
   }
+}
+
+// Flush nan and inf from number to 0
+template <class Number> Number inline flushed(Number input){
+	if(!std::isfinite(input)){
+		return 0.0;
+	}
+
+	return input;
 }
 } // namespace mkutils
