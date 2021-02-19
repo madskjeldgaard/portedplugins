26c830ff48171a9985fa54ce253e403a1a49d7c0
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-02-19 14:17:04 +0100

    update

--- plugins/chaos.hpp
+++ plugins/chaos.hpp
@@ -12,16 +12,17 @@ public:
   enum OutputName { X, Y, Z };
 
   float process();
-  void setup(float initspeed, float initX, float initY, float initZ);
+  void setup(float initspeed, float initX, float initY, float initZ,
+                    float initA, float initB, float initC);
 
-  void setParameter(Parameter type, float param_value);
-  void setSpeed(float speed);
-
-  float getOutput(OutputName outname);
+  inline void setSpeed(float speed);
+  inline float getOutput(OutputName outname);
+  inline void setParameter(Parameter type, float param_value);
 
 private:
+
   float x_{0.f}, y_{0.f}, z_{0.f};
-  float m_a{42.0f}, m_b{3.f}, m_c{28.f};
+  float m_a{0.2f}, m_b{0.2f}, m_c{28.f};
   float m_speed{0.f};
 };
 } // namespace chaos
