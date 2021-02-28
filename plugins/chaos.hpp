a48f5388ce5f782b0d783c8e5731cbf81667e3c4
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-02-28 10:50:29 +0100

    kings dream + broken chua

--- plugins/chaos.hpp
+++ plugins/chaos.hpp
@@ -13,16 +13,42 @@ public:
 
   float process();
   void setup(float initspeed, float initX, float initY, float initZ,
-                    float initA, float initB, float initC);
+             float initA, float initB, float initC);
 
   inline void setSpeed(float speed);
   inline float getOutput(OutputName outname);
   inline void setParameter(Parameter type, float param_value);
 
 private:
-
   float x_{0.f}, y_{0.f}, z_{0.f};
   float m_a{0.2f}, m_b{0.2f}, m_c{28.f};
   float m_speed{0.f};
 };
+
+// Based on
+// https://nathanselikoff.com/training/tutorial-strange-attractors-in-c-and-opengl
+class KingsDreamGen {
+public:
+  void setup(float initX = 0.1f, float initY = 0.1f);
+  float process();
+  float getX();
+  float getY();
+
+private:
+  float m_x{0.0f}, m_y{0.0f}, m_x_state{0.1f}, m_y_state{0.1f};
+};
+
+class ChuaGen {
+public:
+  void setup(float initX = 0.7f, float initY = 0.0f, float initZ = 0.0f);
+  float process();
+  float getX();
+  float getY();
+  float getZ();
+
+private:
+  float m_x{0.f}, m_y{0.f}, m_z{0.f};
+  float m_x_state{0.f}, m_y_state{0.f}, m_z_state{0.f};
+};
+
 } // namespace chaos
