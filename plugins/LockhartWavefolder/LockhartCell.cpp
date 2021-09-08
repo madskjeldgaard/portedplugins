721fc58c9caa3aee8f02b2aa2ffe29a2fcb3acd7
Mads Kjeldgaard <mail@madskjeldgaard.dk>
2021-09-08 20:56:31 +0200

    Back to auto

--- plugins/LockhartWavefolder/LockhartCell.cpp
+++ plugins/LockhartWavefolder/LockhartCell.cpp
@@ -1,24 +1,23 @@
 #include "LockhartCell.hpp"
 #include "SC_InlineUnaryOp.h"
+#include <iostream>
 
 double LockhartCell::lambert_w(double x) {
 
   // Error threshold
-  constexpr double thresh = 10e-10;
+  constexpr auto thresh = 10e-10;
 
   // Initial guess (use previous value)
-  double w = Ln1;
+  auto w = Ln1;
 
   // Haley's method (Sec. 4.2 of the paper)
   for (int i = 0; i < 1000; i++) {
 
-    /* const auto expw = pow(e, w); */
-
-    const double expw = exp(w);
-    const double p = w * expw - x;
-    const double r = (w + 1.0) * expw;
-    const double s = (w + 2.0) / (2.0 * (w + 1.0));
-    const double err = (p / (r - (p * s)));
+    const auto expw = exp(w);
+    const auto p = w * expw - x;
+    const auto r = (w + 1.0) * expw;
+    const auto s = (w + 2.0) / (2.0 * (w + 1.0));
+    const auto err = (p / (r - (p * s)));
 
     if (std::abs(err) < thresh) {
       break;
@@ -52,10 +51,10 @@ and 4, respectively.
 
 */
 double LockhartCell::process(double in1) {
-  double out1 = 0.0;
+  auto out1 = 0.0;
 
   // Constants
-  constexpr double RL = 7.5e3;
+  constexpr auto RL = 7.5e3;
   /* An appropriate RL can be determined empirically. The */
   /* pre- and post- gain can be determined by measuring the */
   /* value of Vout at exactly the folding point. The pre-gain is */
@@ -64,33 +63,40 @@ double LockhartCell::process(double in1) {
   /* chosen, which leads to pre- and post-gains of approx. 1/4 */
   /* and 4, respectively. */
 
-  constexpr double R = 15e3;
-  constexpr double VT = 26e-3;
-  constexpr double Is = 10e-16;
+  constexpr auto R = 15e3;
+  constexpr auto VT = 26e-3;
+  constexpr auto Is = 10e-16;
 
-  constexpr double a = 2.0 * RL / R;
-  constexpr double b = (R + 2.0 * RL) / (VT * R);
-  constexpr double d = (RL * Is) / VT;
+  constexpr auto a = 2.0 * RL / R;
+  constexpr auto b = (R + 2.0 * RL) / (VT * R);
+  constexpr auto d = (RL * Is) / VT;
 
   // Antialiasing error threshold
-  constexpr double thresh = 10e-10;
+  constexpr auto thresh = 10e-10;
 
   // Compute Antiderivative
-  const double l = mkutils::sign(in1);
+  const auto l = mkutils::sign(in1);
 
-  /* const auto u = d * pow(e, l * b * in1); */
-  double u = d * exp(l * b * in1);
-  double Ln = lambert_w(u);
-  const double Fn = (0.5 * VT / b) * (Ln * (Ln + 2.0)) - 0.5 * a * in1 * in1;
+  // @FIXME exp becomes infinity when in1 is too high
+  auto u = d * exp(l * b * in1);
+
+  /*
+  if (std::isnan(u) || std::isinf(u))
+    std::cout << "u: " << u << " l: " << l << " b : " << b << " xn1: " << xn1
+              << " d : " << d << "in1: "<< in1 << std::endl;
+			  */
+
+  auto Ln = lambert_w(u);
+  const auto Fn = (0.5 * VT / b) * (Ln * (Ln + 2.0)) - 0.5 * a * in1 * in1;
 
   // Check for ill-conditioning
   if (std::abs(in1 - xn1) < thresh) {
 
     // Compute Averaged Wavefolder Output
-    const double xn = 0.5 * (in1 + xn1);
-    /* u = d * pow(e, l * b * xn); */
+    const auto xn = 0.5 * (in1 + xn1);
 
     u = d * exp(l * b * xn);
+
     Ln = lambert_w(u);
     out1 = l * VT * Ln - a * xn;
 
